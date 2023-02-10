4ee79f12be369f372fb9bd990a4d70ced5f71a3c
ajadreddy007 <ajad.edavelly@testaing.com>
2023-02-10 19:18:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState,useRef,useEffect} from 'react'
 function App() {
+  const [count,setCount] = useState(0)
+  
+  const inc = () => {
+    setCount(count+1)
+  }
+  const dec = () => {
+    setCount(count-1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>a</p>
+        <p>{count}</p>
+        <button onClick={inc} >+</button>
+        <button onClick={dec}>-</button>
+      </div>
+      <div>
+        <p>b</p>
+        <p>{count}</p>
+        <button onClick={inc} >+</button>
+        <button onClick={dec}>-</button>
+      </div>
+      <div>
+        <p>c</p>
+        <p>{count}</p>
+        <button onClick={inc} >+</button>
+        <button onClick={dec}>-</button>
+      </div>
     </div>
   );
 }
